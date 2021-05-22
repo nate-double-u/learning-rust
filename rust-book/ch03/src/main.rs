a6fbe086c46f33c493cdf859ea99a2b6da59db52
Nate W <4453979+nate-double-u@users.noreply.github.com>
2021-05-22 14:29:43 -0700

    ch03 shadowing example
    Signed-off-by: Nate W <4453979+nate-double-u@users.noreply.github.com>

--- rust-book/ch03/src/main.rs
+++ rust-book/ch03/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    println!("the value of x is: {}", x);
 }
 
