ab3c382033af25986bdcb645a950500cd70c3eb7
Nate W <4453979+nate-double-u@users.noreply.github.com>
2021-05-22 16:40:11 -0700

    ch03 invalid array element access sample
    Signed-off-by: Nate W <4453979+nate-double-u@users.noreply.github.com>

--- rust-book/ch03/samples/src/main.rs
+++ rust-book/ch03/samples/src/main.rs
@@ -1,27 +1,11 @@
 
-use std::io;
-
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("Hello, world!");
 
-    println!("Please enter an array index.");
+    another_function();
+}
 
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-
-    println!(
-        "The value of the element at index {} is: {}",
-        index, element
-    );
+fn another_function() {
+    println!("Another function.");
 }
 
