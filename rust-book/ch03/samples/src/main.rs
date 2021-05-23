e283318c4619c92ddf546e569e5049d056badd66
Nate W <4453979+nate-double-u@users.noreply.github.com>
2021-05-22 17:19:54 -0700

    ch03 loop sample
    Signed-off-by: Nate W <4453979+nate-double-u@users.noreply.github.com>

--- rust-book/ch03/samples/src/main.rs
+++ rust-book/ch03/samples/src/main.rs
@@ -1,11 +1,8 @@
 
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
-}
-
-fn another_function() {
-    println!("Another function.");
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
 
